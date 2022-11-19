38d32a2c5b4625b356c8e66daa60b88105bf1e34
tytti <tytti.m.j.vilen@gmail.com>
2022-11-19 19:54:24 +0200

    initial commitment

--- tehtava2.py
+++ tehtava2.py
@@ -1,20 +1,35 @@
-from flask import Flask, render_template, request
-from flask_mysqldb import MySQL
+from flask import Flask
+import mysql.connector
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'vaahtokarkki'
-app.config['MYSQL_DB'] = 'flask'
+@app.route('/kentta/<ident>')
+def kentta(ident):
+    sql = f'''SELECT airport.name, municipality FROM airport 
+    WHERE ident= "{ident}";'''
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if kursori.rowcount >0:
+        for rivi in tulos:
+            vastaus = {
+                "ICAO" : ident,
+                "name" : rivi[0],
+                "municipality" : rivi[1]
+            }
+    return vastaus
+    kentta(ident)
 
-mysql = MySQL(app)
 
-@app.route('/kenttä')
+yhteys = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='fl1ght_game',
+         user='root',
+         password='vaahtokarkki',
+         autocommit=True
+         )
 
-
-cursor = mysql.connection.cursor()
-
-cursor.execute('''''')
 
 if __name__ == '__main__':
-    app.run(use_reloader=True, host='127.0.0.1', port=3000)
+    app.run(use_reloader = True, host = '127.0.0.1', port=3000)
+
