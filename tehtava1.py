a7345f5dcdb39f0c5b417090e747a24040c86d56
tytti <tytti.m.j.vilen@gmail.com>
2022-11-20 11:56:26 +0200

    initial commitment

--- tehtava1.py
+++ tehtava1.py
@@ -1,35 +1,26 @@
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
-@app.route('/alku')
-def alku():
-    args = request.args
-    Number = float(args.get("Number"))
+@app.route('/Alku/<Number>')
+def Alku(Number):
     parit = []
     arvo = 0
-    while arvo < Number:
+    while arvo < int(Number):
         arvo += 1
-        if Number % arvo == 0:
+        if int(Number) % arvo == 0:
             parit.append(arvo)
     if len(parit) <=2:
         vastaus = {
-            "Number": (f'{Number:.0f}'),
+            "Number": Number,
             "isPrime" : True
         }
     else:
         vastaus = {
-            "Number" : (f'{Number:.0f}'),
+            "Number" : Number,
             "isPrime" : False
         }
 
-    return vastaus
+    return str(vastaus)
 
 if __name__ == '__main__':
     app.run(use_reloader = True, host = '127.0.0.1', port=3000)
-
-
-
-#Toteuta Flask-taustapalvelu, joka ilmoittaa, onko parametrina saatu luku
-#alkuluku vai ei. Hyödynnä toteutuksessa aiempaa tehtävää, jossa alkuluvun testaus tehtiin.
-#Esimerkiksi lukua 31 vastaava GET-pyyntö annetaan muodossa: http://127.0.0.1:3000/alkuluku/31.
-# Vastauksen on oltava muodossa: {"Number":31, "isPrime":true}.
